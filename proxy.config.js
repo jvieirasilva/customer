81110d4fa5e2cfb67ae4f58ebc08b19d67423f24
Jose Neto <j.vieirasilva@gmail.com>
2019-10-14 17:26:48 -0300

    zerando

--- proxy.config.js
+++
@@ -1,9 +0,0 @@
-const proxy = [
-    {
-      context: '/api',
-      target: 'http://localhost:8080/',
-      secure: false,
-      logLevel: 'debug'
-    }
-  ];
-  module.exports = proxy;
--- src/app/address.model.ts
+++
@@ -1,9 +0,0 @@
-
-export class Address {
-    addType    : string;
-    street     : string; 
-    complement : string;
-    city       : string;
-    state      : string;
-    zipCode    : string;
-}
